81757e597396594d43761af555f595b174cd2d12
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-03-13 21:47:44 +0530

    Time: 17 ms (44.57%), Space: 46.1 MB (46.67%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,15 +1,29 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
+        // Without using Stack - O(n^2)
+        // int n = nums.length;
+        // int[] ans = new int[n];
+        // for (int i = 0; i < n; i++) {
+        //     ans[i] = -1;
+        //     for (int j = i + 1; j < i + n; j++) {
+        //         if (nums[j % n] > nums[i]) { // 
+        //             ans[i] = nums[j % n];
+        //             break;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        // Using Stack - O(n)
         int n = nums.length;
         int[] ans = new int[n];
-        for (int i = 0; i < n; i++) {
-            ans[i] = -1;
-            for (int j = i + 1; j < i + n; j++) {
-                if (nums[j % n] > nums[i]) {
-                    ans[i] = nums[j % n];
-                    break;
-                }
+        Stack<Integer> st = new Stack<>();
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && nums[i % n] >= st.peek()) {
+                st.pop();
             }
+            ans[i % n] = st.isEmpty() ? -1 : st.peek();
+            st.push(nums[i % n]);
         }
         return ans;
     }
