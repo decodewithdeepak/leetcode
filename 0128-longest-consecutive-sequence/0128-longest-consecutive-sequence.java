f54cc9ff2f740d931a2588d0db53133f9f5f5450
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-08-04 00:53:35 +0530

    Time: 26 ms (75.16%), Space: 67.7 MB (10.56%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,49 +1,21 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-
-        // Bruteforce Approach : Sort the array and find the longest consecutive sequence
-        // Time Complexity : O(nlogn) + O(n) = O(nlogn)
-
-        // Arrays.sort(nums);
-        // int longestStreak = 1;
-        // int currentStreak = 1;
-
-        // for (int i = 1; i < nums.length; i++) {
-        //     if (nums[i] != nums[i - 1]) {
-        //         if (nums[i] == nums[i - 1] + 1) {
-        //             currentStreak ++;
-        //         } else {
-        //             longestStreak = Math.max(longestStreak, currentStreak);
-        //             currentStreak = 1;
-        //         }
-        //     }
-        // }
-
-        // return Math.max(longestStreak, currentStreak);
-        
-        // Optimized Approach : Using HashSet
-        // Time Complexity : O(n)
-
-        HashSet<Integer> hs = new HashSet<>();
-        for (int num : nums) hs.add(num);
-
-        int longestStreak = 0;
-
-        for (int num : nums) {
-            if (!hs.contains(num - 1)) { // num is starting point of a sequence
-                int currentNum = num;
-                int currentStreak = 1;
-
-                while (hs.contains(currentNum + 1)) {  // Check for next consecutive elements
-                    currentStreak ++;
-                    currentNum ++;
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        int maxStreak = 0;
+        for(int num : set){
+            if(!set.contains(num-1)){ // num is starting point of a sequence
+                int currNum = num;
+                int currStreak = 1; // length of current consecutive sequence
+                while(set.contains(currNum+1)){
+                    currStreak++;
+                    currNum++;
                 }
-
-                longestStreak = Math.max(longestStreak, currentStreak);
+                maxStreak = Math.max(maxStreak, currStreak);
             }
         }
-
-        return longestStreak;
-
+        return maxStreak;
     }
 }
