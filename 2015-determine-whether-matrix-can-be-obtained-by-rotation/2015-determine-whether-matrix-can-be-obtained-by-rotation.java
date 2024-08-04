d2b0eea69b940fe77efcf0c163a66916f637d769
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-08-05 00:50:16 +0530

    Time: 1 ms (49.7%), Space: 41.6 MB (53.59%) - LeetHub

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/2015-determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/2015-determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -1,29 +1,21 @@
-import java.util.Arrays;
-
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
         int n = mat.length;
-        int[][] rotated = mat;
-        // Try all 4 rotations
-        for (int k = 0; k < 4; k++) {
-            if (Arrays.deepEquals(rotated, target)) {
-                return true;
-            }
-            rotated = rotate90(rotated);
-        }
-        return false;
-    }
-    
-    // Helper function to rotate the matrix by 90 degrees
-    public int[][] rotate90(int[][] mat) {
-        int n = mat.length;
-        int[][] rotated = new int[n][n];
+        
+        boolean match0 = true;
+        boolean match90 = true;
+        boolean match180 = true;
+        boolean match270 = true;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                rotated[j][n - 1 - i] = mat[i][j];
+                if (target[i][j] != mat[i][j]) match0 = false;
+                if (target[i][j] != mat[n - j - 1][i]) match90 = false;
+                if (target[i][j] != mat[n - i - 1][n - j - 1]) match180 = false;
+                if (target[i][j] != mat[j][n - i - 1]) match270 = false;
             }
         }
-        return rotated;
-    }
 
+        return match0 || match90 || match180 || match270;
+    }
 }
