de66295fe69e014a89fba613a993cb89e2af31d3
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-08-08 09:53:40 +0530

    Time: 183 ms (27.5%), Space: 46.9 MB (12.18%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,17 +1,18 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        // Brute Force Approach- O(n^2)
+        // Optimized Approach- O(n)
         int n = nums.length;
-        int count = 0;
-        for(int start=0; start<n; start++){
-            int sum = 0;
-            for(int end=start; end<n; end++){
-                sum = sum + nums[end];
-                if(sum == k){
-                    count++;
-                }
+        int prefSum = 0, count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>(); 
+        // Key - Prefix Sum, Value - Frequency
+        map.put(0, 1); // 0 sum has occurred 1 time
+        for(int i=0; i<n; i++){
+            prefSum = prefSum + nums[i];
+            if(map.containsKey(prefSum - k)){
+                count = count + map.get(prefSum - k);
             }
+            map.put(prefSum, map.getOrDefault(prefSum, 0) + 1);
         }
-        return count;   
+        return count;
     }
 }
