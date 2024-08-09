297348ac69265af2622b79dbb089a37b001dd637
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-08-09 20:13:41 +0530

    Time: 0 ms (100%), Space: 45.7 MB (40.65%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -4,22 +4,10 @@ class Solution {
         int end = nums.length - 1;
 
         while (start <= end) {
-            // Find the middle element
-            int mid = start + (end - start) / 2; // To avoid integer overflow -> Better than (start + end) / 2
-
-            // Check if the middle element is the target
-            if (target == nums[mid]) {
-                return mid;
-            }
-
-            // If the target is greater than the middle element -> Ignore the left half
-            if (target > nums[mid]) {
-                start = mid + 1;
-            } 
-            // If the target is less than the middle element -> Ignore the right half
-            else {
-                end = mid - 1;
-            }
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) return mid; // Target found
+            else if (nums[mid] < target) start = mid + 1; // Target is in the right half
+            else end = mid - 1; // Target is in the left half
         }
         return -1;
     }
