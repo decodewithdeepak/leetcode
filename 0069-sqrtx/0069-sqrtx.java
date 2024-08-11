5eec3f717b6158fd6488d7f1f5f988fef88c0899
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-08-11 10:09:34 +0530

    Time: 54 ms (5.01%), Space: 41.4 MB (6.38%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,16 +1,9 @@
 class Solution {
     public int mySqrt(int x) {
-
-        int start = 1, end = x, ans = 0;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            
-            if (mid <= x / mid) {
-                ans = mid;
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
+        // Bruteforce - O(sqrt(n))
+        int ans = 0;
+        for (int i = 1; i <= x/i; i++) {
+            ans = i;
         }
         return ans;
     }
