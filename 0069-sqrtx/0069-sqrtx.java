ed44c471f01d168a033b4f815e20820c108e47e9
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-08-11 13:34:28 +0530

    Time: 1 ms (86%), Space: 40.6 MB (79.65%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -2,18 +2,19 @@ class Solution {
     public int mySqrt(int x) {
         // Binary Search - O(log x)
         int start = 1, end = x;
-        int ans = 0;
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (mid == x / mid) return mid;
-            else if (mid < x / mid) {
+            long square = (long) mid * mid;
+
+            if (square == x) {
+                return mid;
+            } else if (square < x) {
                 start = mid + 1;
-                ans = mid;
-            } else { // mid > x / mid
+            } else { // square > x
                 end = mid - 1;
             }
         }
-        return ans;
+        return end; // If no perfect square is found, return the floor value
     }
 }
