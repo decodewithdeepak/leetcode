e1bc96495627e92388accf0a85de5d59321f83f8
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-08-16 14:30:14 +0530

    Time: 27 ms (26.55%), Space: 44.9 MB (75.46%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,23 +1,31 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int low = 1;
-        int high = 1000000000;
+       // Binary Search - O(n * log m), where n = number of piles, m = max bananas in a pile
+        // We will apply binary search on eating speeds: 1 2 3 4 5 6 7 8 9 10 11 ... maxPile
+
+        int maxPile = 0; // max bananas in a pile
+        for (int pile : piles) {
+            maxPile = Math.max(maxPile, pile);
+        }
+        // range of eating speeds = 1 to maxPile
+
+        int low = 1, high = maxPile;
+
+        if (piles.length == h) return high;
+
         while (low < high) {
-            int mid = (low+high)/2;
-            // if she can't eat all, mid is too low
-            if (!canEatAll(mid, piles, h)) low = mid+1;
-            else high = mid;
+            int mid = low + (high - low) / 2;
+            int hours = 0;
+            for (int pile : piles) {
+                hours += Math.ceil((double) pile / mid); 
+            }
+
+            if (hours <= h) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
         }
         return low;
-    }
-
-    private boolean canEatAll(long k, int[] piles, int h) {
-        int hours = 0;
-        for (int pile: piles) {
-            hours += (int) Math.ceil((double)pile/k);
-
-            if (hours > h) return false;
-        }
-        return true;
     }
 }
