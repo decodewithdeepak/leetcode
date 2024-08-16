5bb6f369b1f9aa7da76212bd6a9645ed21641354
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-08-16 14:27:23 +0530

    Time: 28 ms (17.16%), Space: 45 MB (44.04%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,23 +1,13 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-       // Binary Search - O(n * log m), where n = number of piles, m = max bananas in a pile
-        // We will apply binary search on eating speeds: 1 2 3 4 5 6 7 8 9 10 11 ... maxPile
-
-        int maxPile = 0; // max bananas in a pile
-        for (int pile : piles) {
-            maxPile = Math.max(maxPile, pile);
-        }
-        // range of eating speeds = 1 to maxPile
-
-        int low = 1, high = maxPile;
-
-        if (piles.length == h) return high;
+        int low = 1, high = 1_000_000_000; // 10^9
 
         while (low < high) {
             int mid = low + (high - low) / 2;
             int hours = 0;
+
             for (int pile : piles) {
-                hours += Math.ceil((double) pile / mid); 
+                hours += Math.ceil((double) pile / mid);
             }
 
             if (hours <= h) {
