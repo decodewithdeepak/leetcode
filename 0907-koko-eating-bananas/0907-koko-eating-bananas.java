8abebda94da606539df8fd7a7ef8183085d13112
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-08-16 14:29:01 +0530

    Time: 19 ms (63.21%), Space: 44.8 MB (87.82%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,21 +1,23 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int low = 1, high = 1_000_000_000; // 10^9
-
+        int low = 1;
+        int high = 1000000000;
         while (low < high) {
-            int mid = low + (high - low) / 2;
-            int hours = 0;
-
-            for (int pile : piles) {
-                hours += Math.ceil((double) pile / mid);
-            }
-
-            if (hours <= h) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
+            int mid = (low+high)/2;
+            // if she can't eat all, mid is too low
+            if (!canEatAll(mid, piles, h)) low = mid+1;
+            else high = mid;
         }
         return low;
+    }
+
+    private boolean canEatAll(long k, int[] piles, int h) {
+        int hours = 0;
+        for (int pile: piles) {
+            hours += (int) Math.ceil((double)pile/k);
+
+            if (hours > h) return false;
+        }
+        return true;
     }
 }
