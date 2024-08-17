e908a868f70ca65b54a915517b8f02ee91ce401c
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-08-17 20:07:18 +0530

    Time: 16 ms (30.4%), Space: 42.9 MB (10.62%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,15 +1,34 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) return false;
-        int[] sMap = new int[128]; // map to store the mapping of characters from s to t
-        int[] tMap = new int[128]; // map to store the mapping of characters from t to s
-        for (int i = 0; i < s.length(); i++) {
-            char sChar = s.charAt(i);
-            char tChar = t.charAt(i);
-            if (sMap[sChar] != tMap[tChar]) return false; // if the mapping is different
-            sMap[sChar] = i + 1; // store the index of the character
-            tMap[tChar] = i + 1; // store the index of the character
+        if (s.length() != t.length()) {
+            return false;
         }
+
+        HashMap<Character, Character> mapST = new HashMap<>();
+        HashMap<Character, Character> mapTS = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if (mapST.containsKey(c1)) {
+                if (mapST.get(c1) != c2) {
+                    return false;
+                }
+            } else {
+                mapST.put(c1, c2);
+            }
+
+            if (mapTS.containsKey(c2)) {
+                if (mapTS.get(c2) != c1) {
+                    return false;
+                }
+            } else {
+                mapTS.put(c2, c1);
+            }
+        }
+
         return true;
     }
+
 }
