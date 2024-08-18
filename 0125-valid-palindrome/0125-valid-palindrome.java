3ade642639c1721488b97097e7c8f11ba587b13b
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-08-18 12:52:52 +0530

    Time: 2 ms (99.12%), Space: 42.9 MB (75.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,15 +1,20 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder cleaned = new StringBuilder();
-
-        for (char c : s.toCharArray()) {
-            if (Character.isLetterOrDigit(c)) {
-                cleaned.append(Character.toLowerCase(c));
+        // Two Pointer Approach - TC: O(n) | SC: O(1)
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            char lCh = s.charAt(left);
+            char rCh = s.charAt(right);
+            if (!Character.isLetterOrDigit(lCh)) {
+                left++;
+            } else if (!Character.isLetterOrDigit(rCh)) {
+                right--;
+            } else {
+                if (Character.toLowerCase(lCh) != Character.toLowerCase(rCh)) return false;
+                left++;
+                right--;
             }
         }
-
-        String cleanedStr = cleaned.toString();
-        String reversedStr = cleaned.reverse().toString();
-        return cleanedStr.equals(reversedStr);
+        return true;
     }
 }
