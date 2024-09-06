f588458c709d4de762490289589857ead5d68f14
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-09-06 19:29:24 +0530

    Time: 1 ms (99.78%), Space: 52.8 MB (61.68%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,11 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // O(n) approach - Moore's Voting Algorithm
-        // Rules-
-        // 1. Find candidate to be majority element
-        // 2. Check if candidate is majority element
-
         int n = nums.length;
         int candidate = 0, count = 0;
-        for(int i=0; i<n; i++){
-            if(count == 0){ // new candidate
-                candidate = nums[i];
-            }
-            if(candidate == nums[i]){ // same candidate
-                count++;
-            } else { // different candidate
-                count--;
-            }
+        for(int i = 0; i<n ; i++){
+            if (count == 0) candidate = nums[i]; // new element
+            if (candidate == nums[i]) count++; // same element 
+            else count--; // diff element
         }
         return candidate;
     }
