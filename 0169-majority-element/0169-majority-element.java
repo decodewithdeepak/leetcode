4263c0e8c66e2256adda95cf0209239351021a07
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-09-09 18:54:19 +0530

    Time: 4 ms (53%), Space: 52.9 MB (52.35%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,12 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        int candidate = 0, count = 0;
-        for(int i = 0; i<n ; i++){
-            if (count == 0) candidate = nums[i]; // new element
-            if (candidate == nums[i]) count++; // same element 
-            else count--; // diff element
-        }
-        return candidate;
+        // O(nlogn) approach - Sorting
+        Arrays.sort(nums);
+        return nums[nums.length/2];
     }
 }
