457fcf98657bd2ea89983ad073b9a69e4fb8a50f
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-09-09 22:29:47 +0530

    Time: 2 ms (99.46%), Space: 48 MB (51.64%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,17 +1,47 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        // Using HashMap - O(n)
+        // Extended Moore's Voting Algorithm - O(n)
         List<Integer> ans = new ArrayList<>();
         int n = nums.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<n; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-        for(int key : map.keySet()){
-            if(map.get(key) > n/3){
-                ans.add(key);
+
+        // Find potential candidates
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
+        
+        for (int i = 0; i < n; i++) {
+            // same candidate
+            if (nums[i] == candidate1) {
+                count1++;
+            } else if (nums[i] == candidate2) {
+                count2++;
+            } 
+            // new candidate
+            else if (count1 == 0) {
+                candidate1 = nums[i];
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = nums[i];
+                count2 = 1;
+            } 
+            // different candidate
+            else {
+                count1--;
+                count2--;
             }
         }
+
+        // Verify the candidates
+        count1 = 0;
+        count2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == candidate1) count1++;
+            else if (nums[i] == candidate2) count2++;
+        }
+
+        // Add valid candidates to the result
+        if (count1 > n / 3) ans.add(candidate1);
+        if (count2 > n / 3) ans.add(candidate2);
+
         return ans;
     }
 }
