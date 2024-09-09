0bca215780fc233c2868af672e6664fecaa7de9b
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-09-09 19:10:32 +0530

    Time: 4 ms (56.77%), Space: 48.3 MB (21.44%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,22 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        // Bruteforce approach - O(n^2) - TLE
-        int n = nums.length;
+        // Using Sorting - O(nlogn)
+        Arrays.sort(nums);
         List<Integer> ans = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            int count = 0;
-            for(int j=0; j<n; j++){
-                if(nums[i] == nums[j]){
-                    count++;
+        int n = nums.length;
+        int count = 1;
+        for(int i=1; i<n; i++){
+            if(nums[i] == nums[i-1]){
+                count++;
+            } else {
+                if(count > n/3){
+                    ans.add(nums[i-1]);
                 }
+                count = 1;
             }
-            if(count > n/3 && !ans.contains(nums[i])){
-                ans.add(nums[i]);
-            }
+        }
+        if(count > n/3){
+            ans.add(nums[n-1]);
         }
         return ans;
     }
