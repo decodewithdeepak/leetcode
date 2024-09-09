98699c38ecc1b62af536833dfe68ed0babc96d24
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-09-09 18:58:51 +0530

    Time: 13 ms (29.2%), Space: 49.2 MB (89.44%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,7 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // O(nlogn) approach - Sorting
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        // HashMap - O(n) approach
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // key -> element, value -> frequency
+        for(int i=0; i<n; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        for(int key : map.keySet()){
+            if(map.get(key) > n/2){
+                return key;
+            }
+        }
+        return -1; // no majority element
     }
 }
