8dd2691dc3bf06723c1cc4e51404ab62072518ea
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-09-11 01:27:12 +0530

    Time: 0 ms (100%), Space: 41 MB (93.16%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,35 +1,26 @@
 class MyStack {
-        Queue<Integer> q; // Using a single queue
+    Queue<Integer> q; // Using a single queue
 
-        public MyStack() {
-            q = new LinkedList<>();
-        }
+    public MyStack() {
+        q = new LinkedList<>();
+    }
 
-        public void push(int x) {
-            q.add(x); // add new element 
-            for (int i = 0; i < q.size() - 1; i++) { // rotate queue
-                q.add(q.remove());
-            }
+    public void push(int x) {
+        q.add(x); // add new element
+        for (int i = 0; i < q.size() - 1; i++) { // rotate queue
+            q.add(q.remove());
         }
+    }
 
-        public int pop() {
-            return q.remove();
-        }
+    public int pop() {
+        return q.remove();
+    }
 
-        public int top() {
-            return q.peek();
-        }
+    public int top() {
+        return q.peek();
+    }
 
-        public boolean empty() {
-            return q.isEmpty();
-        }
+    public boolean empty() {
+        return q.isEmpty();
+    }
 }
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack obj = new MyStack();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.top();
- * boolean param_4 = obj.empty();
- */
