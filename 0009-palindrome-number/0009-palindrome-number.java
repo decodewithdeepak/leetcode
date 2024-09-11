de88c463dce00c16df28caf137d7e1cea9330637
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-09-11 11:24:00 +0530

    Time: 7 ms (15.89%), Space: 43.6 MB (90.09%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) return false; // negative no.
+        if (x < 0) return false; // negative no
 
-        int temp = x;
-        int rev = 0;
-        while (temp != 0) {
-            int ld = temp % 10;
-            rev = rev * 10 + ld;
-            temp = temp / 10;
+        String str = Integer.toString(x);
+        int i = 0, j = str.length()-1; // two pointers
+
+        while(i<j){
+            if (str.charAt(i) != str.charAt(j)) return false;
+            i++;
+            j--;
         }
-        if (rev == x) return true;
-        else return false;
+        return true;
     }
 }
