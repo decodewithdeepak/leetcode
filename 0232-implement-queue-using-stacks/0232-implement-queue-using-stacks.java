714fd8fa0b012bfcbbe839500bbc96a875cca408
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-09-12 10:14:08 +0530

    Time: 0 ms (100%), Space: 41.7 MB (11.83%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -39,12 +39,3 @@ class MyQueue {
         return s1.isEmpty() && s2.isEmpty();
     }
 }
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
- */
