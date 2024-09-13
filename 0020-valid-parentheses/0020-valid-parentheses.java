15e5872d6960675711bcc4862c584a2b6d9a7e11
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-09-13 19:12:16 +0530

    Time: 1 ms (98.61%), Space: 41.2 MB (85.1%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,26 +3,16 @@ class Solution {
         Stack<Character> st = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            // Push opening brackets to stack
-            if (c == '(' || c == '{' || c == '[') {
-                st.push(c);
-            } 
-            // Pop closing brackets from stack if they match with top of stack
-            else if (c == ')' && !st.isEmpty() && st.peek() == '(') {
-                st.pop();
-            } 
-            else if (c == '}' && !st.isEmpty() && st.peek() == '{') {
-                st.pop();
-            } 
-            else if (c == ']' && !st.isEmpty() && st.peek() == '[') {
-                st.pop();
-            } 
-            // If closing bracket does not match with top of stack, return false
-            else {
-                return false;
-            }
+            // push opening brackets to stack
+            if (c == '(' || c == '{' || c == '[')  st.push(c);
+            // pop corresponding opening bracket if closing bracket is found at top
+            else if (c == ')' && !st.isEmpty() && st.peek() == '(') st.pop();
+            else if (c == '}' && !st.isEmpty() && st.peek() == '{') st.pop();
+            else if (c == ']' && !st.isEmpty() && st.peek() == '[') st.pop();
+            // closing bracket found at top but no corresponding opening bracket
+            else return false;
         }
-        // If stack is empty at the end, return true
+        // stack should be empty if all brackets are balanced
         return st.isEmpty();
     }
 
