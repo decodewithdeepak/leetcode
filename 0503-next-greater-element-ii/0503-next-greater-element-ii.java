6c5e998a639349defa4de5425eee951bbad43012
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-09-17 11:19:31 +0530

    Time: 17 ms (48.71%), Space: 46.1 MB (49.53%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,16 +1,29 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        // Brute Force Approach - O(n^2)
+        // Without using Stack - O(n^2)
+        // int n = nums.length;
+        // int[] ans = new int[n];
+        // for (int i = 0; i < n; i++) {
+        //     ans[i] = -1;
+        //     for (int j = i + 1; j < i + n; j++) {
+        //         if (nums[j % n] > nums[i]) { // 
+        //             ans[i] = nums[j % n];
+        //             break;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        // Using Stack - O(n)
         int n = nums.length;
         int[] ans = new int[n];
-        for (int i = 0; i < n; i++) {
-            ans[i] = -1;
-            for (int j = i + 1; j < i + n; j++) {
-                if (nums[j % n] > nums[i]) { // j%n to handle circular array
-                    ans[i] = nums[j % n];
-                    break;
-                }
+        Stack<Integer> st = new Stack<>();
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && nums[i % n] >= st.peek()) {
+                st.pop();
             }
+            ans[i % n] = st.isEmpty() ? -1 : st.peek();
+            st.push(nums[i % n]);
         }
         return ans;
     }
