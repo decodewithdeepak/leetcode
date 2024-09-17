2a0d6f907ab185c14f7aff8d6fba833b7c2d706f
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-09-17 11:22:24 +0530

    Time: 17 ms (48.71%), Space: 45.9 MB (73.53%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,30 +1,19 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        // Without using Stack - O(n^2)
-        // int n = nums.length;
-        // int[] ans = new int[n];
-        // for (int i = 0; i < n; i++) {
-        //     ans[i] = -1;
-        //     for (int j = i + 1; j < i + n; j++) {
-        //         if (nums[j % n] > nums[i]) { // 
-        //             ans[i] = nums[j % n];
-        //             break;
-        //         }
-        //     }
-        // }
-        // return ans;
-
         // Using Stack - O(n)
         int n = nums.length;
         int[] ans = new int[n];
         Stack<Integer> st = new Stack<>();
-        for (int i = 2 * n - 1; i >= 0; i--) {
+
+        for (int i = 2 * (n-1); i >= 0; i--) {
             while (!st.isEmpty() && nums[i % n] >= st.peek()) {
                 st.pop();
             }
-            ans[i % n] = st.isEmpty() ? -1 : st.peek();
+            if (st.isEmpty()) ans[i % n] = -1;
+            else ans[i % n] = st.peek();
             st.push(nums[i % n]);
         }
+
         return ans;
     }
 }
