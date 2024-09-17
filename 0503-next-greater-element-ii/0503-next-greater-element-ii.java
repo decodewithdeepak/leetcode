1d5e1071cae14139f246109785956e674fd8894f
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-09-17 11:16:43 +0530

    Time: 91 ms (6.04%), Space: 46.2 MB (49.53%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,11 +1,12 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
+        // Brute Force Approach - O(n^2)
         int n = nums.length;
         int[] ans = new int[n];
         for (int i = 0; i < n; i++) {
             ans[i] = -1;
             for (int j = i + 1; j < i + n; j++) {
-                if (nums[j % n] > nums[i]) {
+                if (nums[j % n] > nums[i]) { // j%n to handle circular array
                     ans[i] = nums[j % n];
                     break;
                 }
