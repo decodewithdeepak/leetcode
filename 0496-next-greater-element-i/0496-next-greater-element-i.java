a4f9e302b8faa4a63ef3d5b2733941ef52c19cfc
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-09-17 10:21:28 +0530

    Time: 3 ms (93.17%), Space: 44.2 MB (24.5%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -2,33 +2,23 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         // nums1: query array, nums2: source array (next greater element in nums2 for each element in nums1)
 
-        int ngl[] = nextGreaterElement(nums2);
-
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        // key: element, value: next greater element
-        for (int i = 0; i < nums2.length; i++) {
-            hm.put(nums2[i], ngl[i]);
+        HashMap<Integer, Integer> hm = new HashMap<>(); // key: element, value: next greater element
+        Stack<Integer> st = new Stack<>(); // stack to store elements in decreasing order
+        
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!st.isEmpty() && nums2[i] >= st.peek()) {
+                st.pop();
+            }
+            if (st.isEmpty()) hm.put(nums2[i], -1);
+            else hm.put(nums2[i], st.peek());
+            st.push(nums2[i]);
         }
 
         int[] ans = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
+        for (int i = 0; i < ans.length; i++) {
             ans[i] = hm.get(nums1[i]);
         }
-        return ans;
-    }
-
-    public int[] nextGreaterElement(int[] arr) {
-        int n = arr.length;
-        int[] ans = new int[n];
-        Stack<Integer> st = new Stack<>();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && arr[i] >= st.peek()) {
-                st.pop();
-            }
-            if (st.isEmpty()) ans[i] = -1;
-            else ans[i] = st.peek();
-            st.push(arr[i]);
-        }
+        
         return ans;
     }
 }
