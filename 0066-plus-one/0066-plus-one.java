47aa2b2115030ca5bc3a3c6a9bd0a7bf0e726ea4
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-09-22 00:20:17 +0530

    Time: 0 ms (100%), Space: 41.6 MB (68.44%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -4,13 +4,9 @@ class Solution {
 
         // traverse from right to left
         for(int i = n - 1; i >= 0; i--) {
-            // if digit is less than 9, increment and return
-            if(digits[i] < 9) {
-                digits[i]++;
-                return digits;
-            }
-            // if digit is 9, make it 0
-            else digits[i] = 0;
+            digits[i]++; // add 1 to the digit
+            if (digits[i] < 10) return digits; // if no carry, return the array
+            else digits[i] = 0; // if 10, make it 0 and carry 1 to next digit
         }
 
         // if all digits are 9 => 999 + 1 = 1000
