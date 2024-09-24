7929b546e7f3e4f11636d467062b2cd23263fa33
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-09-24 11:37:08 +0530

    Time: 6 ms (71.14%), Space: 55.2 MB (37.18%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,12 +1,13 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
+        // Using Negation - O(n) time, O(1) space
         List<Integer> dup = new ArrayList<>();
-        HashSet<Integer> set = new HashSet<>();
-        for(int num : nums){
-            if(set.contains(num)){
-                dup.add(num);
+        for(int i = 0; i < nums.length; i++){
+            int idx = Math.abs(nums[i]) - 1; // index of no nums[i]
+            if(nums[idx] < 0){ // if no is already negated then it is duplicate
+                dup.add(Math.abs(nums[i])); // add duplicate no to list
             }
-            set.add(num);
+            nums[idx] = -nums[idx]; // mark no as visited by negating
         }
         return dup;
     }
