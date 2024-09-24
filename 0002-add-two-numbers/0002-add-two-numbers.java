841cd82fd7c1fbafb7924931f4f8264ceeef0123
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-09-24 15:50:25 +0530

    Time: 2 ms (15.33%), Space: 44.1 MB (93.16%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -4,21 +4,31 @@ class Solution {
         // l2:  1->9->7         |  +   791   | + 197
         // sum: 5->8->7->6->9   |  = 96785   | = 58769
 
-        ListNode p1 = l1, p2 = l2; 
-        ListNode dummy = new ListNode(-1);
-        ListNode curr = dummy;
+        ListNode p1 = l1, p2 = l2; // pointers to traverse both lists
+        ListNode dummy = new ListNode(-1); // dummy node
+        ListNode curr = dummy; // current pointer to track the sum
         int carry = 0;
 
-        while (p1 != null || p2 != null || carry != 0) {
+        // add the values of nodes and carry
+        while (p1 != null || p2 != null) {
+            // sum = carry + p1.val + p2.val
             int sum = carry + (p1 != null ? p1.val : 0) + (p2 != null ? p2.val : 0);
-            carry = sum / 10;
-            sum = sum % 10;
+            
+            int ld = sum % 10; // extract last digit of sum
+            carry = sum / 10; // extract carry
 
-            curr.next = new ListNode(sum);
-            curr = curr.next;
+            // make a new node of ld and add it to the list
+            curr.next = new ListNode(ld);
 
+            // move to next nodes
             if (p1 != null) p1 = p1.next;
             if (p2 != null) p2 = p2.next;
+            curr = curr.next;
+        }
+
+        // add a new node of carry if it is not 0
+        if (carry != 0) {
+            curr.next = new ListNode(carry);
         }
 
         return dummy.next; // head of sum
