bc7454225f6ddf77c5d62f5770a9642e53ad45fe
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-09-24 16:30:09 +0530

    Time: 0 ms (100%), Space: 41 MB (84.73%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -10,33 +10,26 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode p1=head;
-        ListNode p2=head;
-        // move p1 and p2 to left and right
-        for(int i=1;i<left;i++){
-            p1=p1.next;
-        }
-        for(int i=1;i<right;i++){
-            p2=p2.next;
-        }
-        
-        // store the values of nodes in an arraylist
-        ArrayList<Integer> arr=new ArrayList<>();
-        // add the values of nodes from left to right
-        ListNode temp=p1;
-        while(temp!=p2.next){
-            arr.add(temp.val);
-            temp=temp.next;
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        // move prev to left-1
+        for (int i = 0; i < left - 1; i++) {
+            prev = prev.next;
         }
 
-        // from right to left, replace the values of nodes with arraylist values
-        int l = arr.size()-1;
-        while(p1!=p2.next){
-            p1.val=arr.get(l);
-            l--;
-            p1=p1.next;
+        ListNode curr = prev.next; // curr will be at left, after prev
+
+        // reverse the nodes from left to right
+        for (int i = 0; i < right - left; i++) {
+            ListNode front = curr.next; // front will be after curr
+            curr.next = front.next;
+            front.next = prev.next;
+            prev.next = front;
         }
 
-        return head;
+        return dummy.next; // head
     }
 }
