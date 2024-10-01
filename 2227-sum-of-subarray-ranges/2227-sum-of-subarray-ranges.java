3e90480ad60da39d728eec8ab1fb6cc504285e60
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-10-01 22:45:25 +0530

    Time: 22 ms (40.69%), Space: 44.8 MB (18.55%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -1,17 +1,59 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
-        // brute force - O(n^2)
+        // Using Stack - O(n)
+
+        // ∑ (max - min) = ∑ max - ∑ min
+        // ∑ max = sum of subarray maximums
+        // ∑ min = sum of subarray minimums
+
+        return sumSubarrayMax(nums) - sumSubarrayMin(nums);
+    }
+
+    private long sumSubarrayMax(int[] arr) {
+        int n = arr.length;
         long sum = 0;
-        int n = nums.length;
-        
+        Stack<Integer> st = new Stack<>();
+
         for (int i = 0; i < n; i++) {
-            int min = nums[i];
-            int max = nums[i];
-            for (int j = i; j < n; j++) {
-                min = Math.min(min, nums[j]);
-                max = Math.max(max, nums[j]);
-                sum += max - min;
+            // Process for maximums
+            while (!st.isEmpty() && arr[i] > arr[st.peek()]) {
+                int j = st.pop();
+                int left = st.isEmpty() ? -1 : st.peek();
+                sum += (long) arr[j] * (j - left) * (i - j);
             }
+            st.push(i);
+        }
+
+        // Clear remaining elements in the stack
+        while (!st.isEmpty()) {
+            int j = st.pop();
+            int left = st.isEmpty() ? -1 : st.peek();
+            sum += (long) arr[j] * (j - left) * (n - j);
+        }
+
+        return sum;
+    }
+
+    private long sumSubarrayMin(int[] arr) {
+        int n = arr.length;
+        long sum = 0;
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            // Process for minimums
+            while (!st.isEmpty() && arr[i] < arr[st.peek()]) {
+                int j = st.pop();
+                int left = st.isEmpty() ? -1 : st.peek();
+                sum += (long) arr[j] * (j - left) * (i - j);
+            }
+            st.push(i);
+        }
+
+        // Clear remaining elements in the stack
+        while (!st.isEmpty()) {
+            int j = st.pop();
+            int left = st.isEmpty() ? -1 : st.peek();
+            sum += (long) arr[j] * (j - left) * (n - j);
         }
 
         return sum;
