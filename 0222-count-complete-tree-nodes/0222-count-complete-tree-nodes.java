b2473d7d3d390ffed84e87498fbf67fdc7e4c438
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-10-01 21:48:02 +0530

    Time: 0 ms (100%), Space: 47 MB (93.88%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -16,10 +16,6 @@
 class Solution {
     public static int countNodes(TreeNode root) {
         if (root == null) return 0;
-
-        int left = countNodes(root.left);
-        int right = countNodes(root.right);
-
-        return left + right + 1;
+        return countNodes(root.left) + countNodes(root.right) + 1;
     }
 }
