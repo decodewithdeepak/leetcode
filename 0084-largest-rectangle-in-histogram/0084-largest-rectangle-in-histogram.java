5478f7a6b04d62c497ba8af297274069b249e68f
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-10-02 16:48:30 +0530

    Time: 119 ms (11.87%), Space: 57.8 MB (24.55%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,22 +1,48 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        // Brute Force Approach - O(n^2)
-        int n = heights.length;
+        // Using Stack - O(n) - Concept of NSE and PSE
         int maxArea = 0;
         
-        for (int i = 0; i < n; i++) {
-            int h = heights[i];
-            int left = i, right = i;
-            while (left >= 0 && heights[left] >= h) {
-                left--;
-            }
-            while (right < n && heights[right] >= h) {
-                right++;
-            }
-            maxArea = Math.max(maxArea, h * (right - left - 1)); 
-            // right - left - 1 -> width of rectangle
+        int[] ps = prevSmaller(heights);
+        int[] ns = nextSmaller(heights);
+
+        for (int i = 0; i < heights.length; i++) {
+            int currArea = heights[i] * (ns[i] - ps[i] - 1);
+            maxArea = Math.max(maxArea, currArea);
         }
 
         return maxArea;
+    }
+
+    private int[] nextSmaller(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[i] <= arr[st.peek()]) {
+                st.pop();
+            }
+            ans[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+    private int[] prevSmaller(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[i] <= arr[st.peek()]) {
+                st.pop();
+            }
+            ans[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        return ans;
     }
 }
