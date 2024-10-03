aed3f1db834b4c1a153b93ea1a5529a641a76495
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-10-03 10:45:26 +0530

    Time: 0 ms (100%), Space: 41.5 MB (45.91%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,27 +1,27 @@
-class Solution {    
+class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> result = new ArrayList<>();
-        
-        int m = matrix.length; // rows
-        int n = matrix[0].length; // columns
 
-        int top = 0, bottom = m - 1;
-        int left = 0, right = n - 1;
+        int r = matrix.length; // rows
+        int c = matrix[0].length; // columns
+
+        int top = 0, bottom = r - 1; // top and bottom row
+        int left = 0, right = c - 1; // left and right column
 
         while (top <= bottom && left <= right) {
-            // Traverse top row : left -> right
+            // Traverse top row -> left to right
             for (int j = left; j <= right; j++) {
                 result.add(matrix[top][j]);
             }
             top++;
 
-            // Traverse right column : top -> bottom
+            // Traverse right column -> top to bottom
             for (int i = top; i <= bottom; i++) {
                 result.add(matrix[i][right]);
             }
             right--;
 
-            // Traverse bottom row : right -> left
+            // Traverse bottom row -> right to left
             if (top <= bottom) {
                 for (int j = right; j >= left; j--) {
                     result.add(matrix[bottom][j]);
@@ -29,7 +29,7 @@ class Solution {
                 bottom--;
             }
 
-            // Traverse left column : bottom -> top
+            // Traverse left column -> bottom to top
             if (left <= right) {
                 for (int i = bottom; i >= top; i--) {
                     result.add(matrix[i][left]);
