590a446dd1b6fa2b1e90f1481a4eb7cc5b5378d7
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-10-11 10:42:03 +0530

    Time: 2 ms (98.8%), Space: 44.9 MB (46.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,8 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         // Using Hashing - O(n) time, O(n) space
+
         int n = nums.length;
         HashMap<Integer, Integer> map = new HashMap<>();
+        // Key - number nums[i], Value - index i
+
         for(int i=0; i<n; i++){
             int diff = target - nums[i];
             if(map.containsKey(diff)){
@@ -10,6 +13,7 @@ class Solution {
             }
             map.put(nums[i], i);
         }
-        return null;
+
+        return new int[]{-1, -1};
     }
 }
