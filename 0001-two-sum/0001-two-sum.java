08e34a84860cd87d7d988c14e43f18ba83397b99
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-10-11 10:40:06 +0530

    Time: 2 ms (98.8%), Space: 44.7 MB (63.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Brute Force Approach - O(n^2)
+        // Using Hashing - O(n) time, O(n) space
         int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i, j};
-                }
+            int diff = target - nums[i];
+            if(map.containsKey(diff)){
+                return new int[]{map.get(diff), i};
             }
+            map.put(nums[i], i);
         }
         return null;
     }
