d291f28b468270711a497c831d3137d7f77a28b4
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-10-20 16:12:20 +0530

    Time: 0 ms (100%), Space: 43 MB (13.22%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,20 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
         if (n == 0) return 1; // base case
-        
-        // recurrence relation :
-        // if n is negative -> x^n = 1 / (x^n)
-        // if n is even -> x^n = x^(n/2) * x^(n/2)
-        // if n is odd -> x^n = x^(n/2) * x^(n/2) * x
 
+        // smaller subproblem
         double smallPow = myPow(x, n / 2);
 
+        // if n is negative -> x^-n = 1 / x^n = (1 / x)^n
         if (n < 0) {
             x = 1 / x;
             n = -n;
         }
 
-        if (n % 2 == 0) return smallPow * smallPow; // even n
-        else return smallPow * smallPow * x; // odd n
+        // if n is even -> x^n = x^(n/2) * x^(n/2)
+        if (n % 2 == 0) return smallPow * smallPow;
+
+        // if n is odd -> x^n = x^(n/2) * x^(n/2) * x
+        else return smallPow * smallPow * x;
     }
 }
