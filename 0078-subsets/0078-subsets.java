bc7da5041e9c281f1a4f5e1e3ba2ffbf1268bb6a
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-11-11 12:19:17 +0530

    Time: 1 ms (54.53%), Space: 42.8 MB (22.1%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -4,12 +4,13 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         result.add(new ArrayList<>()); // add the empty subset
         for (int num : nums) {
-            int size = result.size();
-            for (int i = 0; i < size; i++) {
-                List<Integer> subset = new ArrayList<>(result.get(i));
-                subset.add(num);
-                result.add(subset);
+            List<List<Integer>> newSubsets = new ArrayList<>();
+            for (List<Integer> subset : result) {
+                List<Integer> newSubset = new ArrayList<>(subset);
+                newSubset.add(num);
+                newSubsets.add(newSubset);
             }
+            result.addAll(newSubsets);
         }
         return result;
     }
