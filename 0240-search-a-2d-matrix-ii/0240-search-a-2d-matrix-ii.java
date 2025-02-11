1b2099379144104f654dee7950ee7e5b358eda94
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2025-02-11 13:13:56 +0530

    Time: 11 ms (9.56%), Space: 45.8 MB (79.94%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,17 +1,11 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        // Staircase Search method : TC- O(m+n)
-
-        int m = matrix.length; // rows
-        int n = matrix[0].length; // columns
-        int i = 0, j = n - 1; // start from top-right corner
-
-        while (i < m && j >= 0) {
-            if (matrix[i][j] == target) return true; // target found
-            else if (matrix[i][j] > target) j--; // move left
-            else i++; // move down
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (target == matrix[i][j])
+                    return true;
+            }
         }
-
-        return false; // target not found
+        return false;
     }
 }
