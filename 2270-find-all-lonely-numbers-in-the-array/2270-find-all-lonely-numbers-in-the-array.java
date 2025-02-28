679f6047c53aac95f0f52c142a4963b7057397df
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2025-02-28 22:40:30 +0530

    Time: 33 ms (76.97%), Space: 65.6 MB (50%) - LeetHub

--- 2270-find-all-lonely-numbers-in-the-array/2270-find-all-lonely-numbers-in-the-array.java
+++ 2270-find-all-lonely-numbers-in-the-array/2270-find-all-lonely-numbers-in-the-array.java
@@ -1,13 +1,31 @@
+import java.util.*;
+
 class Solution {
     public List<Integer> findLonely(int[] nums) {
-        ArrayList<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            
-            if ((i == 0 || nums[i] - nums[i - 1] > 1) && (i == nums.length - 1 || nums[i + 1] - nums[i] > 1)) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            // handle single-element case
+            if (n == 1) {
+                list.add(nums[i]);
+                break;
+            }
+            // check first element
+            if (i == 0 && nums[i] + 1 < nums[i + 1]) {
+                list.add(nums[i]);
+            }
+            // check last element
+            else if (i == n - 1 && nums[i] - 1 > nums[i - 1]) {
+                list.add(nums[i]);
+            }
+            // check middle elements
+            else if (i > 0 && i < n - 1 && nums[i] - 1 > nums[i - 1] && nums[i] + 1 < nums[i + 1]) {
                 list.add(nums[i]);
             }
         }
+        
         return list;
     }
 }
