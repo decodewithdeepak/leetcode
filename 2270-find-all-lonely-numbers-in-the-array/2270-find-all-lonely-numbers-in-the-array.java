328490b23bdf509053a1edbf9ae234df71e8e04a
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2025-02-28 22:43:58 +0530

    Time: 75 ms (57.4%), Space: 67 MB (15.27%) - LeetHub

--- 2270-find-all-lonely-numbers-in-the-array/2270-find-all-lonely-numbers-in-the-array.java
+++ 2270-find-all-lonely-numbers-in-the-array/2270-find-all-lonely-numbers-in-the-array.java
@@ -1,31 +1,25 @@
-import java.util.*;
-
 class Solution {
     public List<Integer> findLonely(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        Arrays.sort(nums);
-        int n = nums.length;
+        List<Integer> lonelyNumbers = new ArrayList<>();
+        Map<Integer, Integer> countMap = new HashMap<>();
 
-        for (int i = 0; i < n; i++) {
-            // handle single-element case
-            if (n == 1) {
-                list.add(nums[i]);
-                break;
-            }
-            // check first element
-            if (i == 0 && nums[i] + 1 < nums[i + 1]) {
-                list.add(nums[i]);
-            }
-            // check last element
-            else if (i == n - 1 && nums[i] - 1 > nums[i - 1]) {
-                list.add(nums[i]);
-            }
-            // check middle elements
-            else if (i > 0 && i < n - 1 && nums[i] - 1 > nums[i - 1] && nums[i] + 1 < nums[i + 1]) {
-                list.add(nums[i]);
+        // count frequencies
+        for (int num : nums) {
+            // if present, increment count
+            if (countMap.containsKey(num))
+                countMap.put(num, countMap.get(num) + 1);
+            // if not present, add it
+            else
+                countMap.put(num, 1);
+        }
+
+        // check lonely conditions
+        for (int num : nums) {
+            if (countMap.get(num) == 1 && !countMap.containsKey(num - 1) && !countMap.containsKey(num + 1)) {
+                lonelyNumbers.add(num);
             }
         }
-        
-        return list;
+
+        return lonelyNumbers;
     }
 }
