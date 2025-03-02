721e97cec42c7cf926752b38beeacd74ce7c6769
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2025-03-02 17:56:54 +0530

    Time: 1 ms (100%), Space: 57 MB (75.61%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,18 +1,31 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        
-        for (int num : nums) {
-            if (num > 0) {
-                set.add(num);
+        int n = nums.length;
+        int i = 0;
+
+        // place numbers at their correct index (num -> nums[num - 1])
+        while (i < n) {
+            int newIdx = nums[i] - 1;
+            if (nums[i] > 0 && nums[i] <= n && nums[i] != nums[newIdx]) {
+                swap(nums, i, newIdx);
+            } else {
+                i++;
             }
         }
 
-        int smallest = 1;
-        while (set.contains(smallest)) {
-            smallest++;
+        // find first missing positive
+        for (i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
         }
+        // if all numbers are in place then return n + 1
+        return n + 1;
+    }
 
-        return smallest;
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
