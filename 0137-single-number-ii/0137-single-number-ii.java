17bbb90c1245fd21080aed64b0951b99896031e9
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2025-03-07 12:32:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,12 +1,21 @@
+import java.util.Arrays;
+
 class Solution {
     public int singleNumber(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            int cnt = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[j] == nums[i]) cnt++;
+        Arrays.sort(nums);
+
+        int n = nums.length;
+        // check first element
+        if(nums[0] != nums[1]) return nums[0];
+        // check last element
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+
+        for (int i = 0; i < n; i += 3) {
+            if (nums[i+1] != nums[i]) {
+                return nums[i];
             }
-            if (cnt == 1) return nums[i];
         }
+
         return -1;
     }
 }
