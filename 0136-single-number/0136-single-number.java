1eee8ac574c91970d66234222810498b78751f26
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2025-03-07 12:04:12 +0530

    Time: 361 ms (5.03%), Space: 46.9 MB (5.69%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,10 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int n = nums.length;
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            ans = ans ^ nums[i]; // XOR operation
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j]) count++;
+            }
+            if (count == 1) return nums[i];
         }
-        return ans;
+        return -1;
     }
 }
