ec04340584bb2d9ba2c5386b4a55f636a16f986e
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2025-03-07 12:37:10 +0530

    Time: 4 ms (53.27%), Space: 45.3 MB (79.05%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,17 +1,18 @@
-import java.util.Arrays;
-
 class Solution {
     public int singleNumber(int[] nums) {
         Arrays.sort(nums);
 
         int n = nums.length;
+        // if only one element
+        if (n==1) return nums[0];
         // check first element
-        if(nums[0] != nums[1]) return nums[0];
+        if (nums[0] != nums[1]) return nums[0];
         // check last element
-        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        if (nums[n-1] != nums[n-2]) return nums[n-1];
 
-        for (int i = 0; i < n; i += 3) {
-            if (nums[i+1] != nums[i]) {
+        // check triplets
+        for (int i=0; i<n; i+=3) {
+            if (nums[i] != nums[i+1]) {
                 return nums[i];
             }
         }
