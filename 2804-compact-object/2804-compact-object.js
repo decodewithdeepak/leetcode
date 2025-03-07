424951568c1706f33335bd1c8668c0d5c1b49b7d
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2025-03-07 11:58:47 +0530

    Time: 69 ms (89.91%), Space: 65.4 MB (22.18%) - LeetHub

---
+++ 2804-compact-object/2804-compact-object.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+var compactObject = function (obj) {
+    const compactObj = Array.isArray(obj) ? [] : {};
+    for (const key in obj) {
+        let elem = obj[key];
+        if (elem) {
+            if (typeof elem === "object") elem = compactObject(elem);
+            Array.isArray(obj) ? compactObj.push(elem) : compactObj[key] = elem;
+        }
+    }
+    return compactObj;
+};
