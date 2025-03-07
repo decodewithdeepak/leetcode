1a7f64df218fdbc820a8859410c26103aa999067
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2025-03-07 12:08:09 +0530

    Time: 12 ms (22.6%), Space: 44.9 MB (96.64%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,12 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            int count = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] == nums[j]) count++;
-            }
-            if (count == 1) return nums[i];
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) set.remove(num);
+            else set.add(num);
         }
-        return -1;
+
+        return set.iterator().next();
     }
 }
