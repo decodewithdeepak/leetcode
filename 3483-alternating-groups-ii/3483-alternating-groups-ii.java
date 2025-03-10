842172cb112d10f8f83870f9b2f8f1f0a13a4a81
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2025-03-10 11:02:26 +0530

    Time: 3 ms (86.91%), Space: 63.2 MB (37.33%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.java
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.java
@@ -1,14 +1,20 @@
 class Solution {
     public int numberOfAlternatingGroups(int[] colors, int k) {
         int n = colors.length;
-        int cnt = 1, ans = 0, lastCol = colors[0];
-        for(int i = 1; i<n+k-1; i++){
-            int ind = i%n;
-            if(colors[ind] == lastCol) cnt = 1;
-            else cnt++;
-            if(cnt>=k) ans++;
-            lastCol = colors[ind];
+        int len = 1; // window lemgth
+        int count = 0; // alt groups count
+        int prevCol = colors[0]; // previous color
+
+        for (int i = 1; i < n + k - 1; i++){
+            int idx = i % n; // handle circular index
+            if (colors[idx] == prevCol) len = 1; // if same color appears reset window length
+            else len++; // increase window length
+
+            if (len >= k) count++; // count alt group
+
+            prevCol = colors[idx]; // update prev color
         }
-        return ans;
+
+        return count;
     }
 }
