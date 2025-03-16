66cfeb9af46d1621e9e6ebb214ac761c1319e859
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2025-03-16 19:00:15 +0530

    Time: 27 ms (77.69%), Space: 55.2 MB (85.75%) - LeetHub

--- 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.java
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.java
@@ -1,32 +1,28 @@
 class Solution {
     public long repairCars(int[] ranks, int cars) {
-        long left = 1;
-        long right = (long) ranks[0] * cars * cars;
-        
+        // binary serach on time
+        long left = 1, right = (long) ranks[0] * cars * cars;
+
         for (int rank : ranks) {
             right = Math.min(right, (long) rank * cars * cars);
         }
-        
+
         while (left < right) {
             long mid = left + (right - left) / 2;
-            if (canRepairAllCarsInTime(ranks, cars, mid)) {
-                right = mid;
-            } else {
-                left = mid + 1;
+            long totalCarsRepaired = 0;
+
+            for (int rank : ranks) {
+                totalCarsRepaired += Math.sqrt(mid / rank);
+                if (totalCarsRepaired >= cars) break; // no need to check further
             }
-        }
-        
-        return left;
-    }
-    
-    private boolean canRepairAllCarsInTime(int[] ranks, int cars, long time) {
-        long totalCarsRepaired = 0;
-        for (int rank : ranks) {
-            totalCarsRepaired += Math.sqrt(time / rank);
+
             if (totalCarsRepaired >= cars) {
-                return true;
+                right = mid; // try for a smaller time
+            } else {
+                left = mid + 1; // increase time
             }
         }
-        return false;
+
+        return left; // minimum time required
     }
 }
