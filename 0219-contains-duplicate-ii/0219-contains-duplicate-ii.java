3ed52cc80cf7d9f38304e50653917c7da70633a3
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2025-07-11 16:48:03 +0530

    Time: 1177 ms (6.87%), Space: 57.4 MB (95.74%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,5 +1,6 @@
 public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        // indices (i,j) should differ by k distance
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length && j <= i + k; j++) {
                 if (nums[i] == nums[j]) {
