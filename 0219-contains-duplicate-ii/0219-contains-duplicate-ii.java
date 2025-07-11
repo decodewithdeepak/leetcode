c558929ee2b49f609db5aa0bf9239c6b98f0e77d
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2025-07-11 16:58:41 +0530

    Time: 21 ms (78.58%), Space: 60.4 MB (16.62%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,13 +1,16 @@
-public class Solution {
+class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // indices (i,j) should differ by k distance
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length && j <= i + k; j++) {
-                if (nums[i] == nums[j]) {
-                    return true; // found duplicate within k distance
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) { // i -> currIndex
+            if (map.containsKey(nums[i])) { // duplicate
+                int prevIndex = map.get(nums[i]);
+                if (i - prevIndex <= k) {
+                    return true; // with condition
                 }
             }
+            map.put(nums[i], i); // store or update index
         }
+
         return false;
     }
 }
